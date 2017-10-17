8ee1562f6d8e90d2dfe0a6f8781c956a87aa79ae
venkatsai2727 <32855111+venkatsai2727@users.noreply.github.com>
2017-10-17 14:19:10 -0500

    Add files via upload

---
+++ src/main/java/hello/storage/StorageService.java
@@ -0,0 +1,26 @@
+package hello.storage;
+
+import org.springframework.core.io.Resource;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.nio.file.Path;
+import java.util.stream.Stream;
+
+public interface StorageService {
+
+    void init();
+
+    void store(MultipartFile file);
+
+    void store(String metaData);
+
+    Stream<Path> loadAll();
+
+    Path load(String filename);
+
+    Resource loadAsResource(String filename);
+
+    void deleteAll();
+
+
+}
